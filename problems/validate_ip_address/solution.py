3e8377dc07269aee8c901304515b433fe5e9d56d
Arjun <iarjun.sahlot@gmail.com>
2020-05-07 01:47:43 +0000

    Sync LeetCode submission - Validate IP Address (python3)

---
+++ problems/validate_ip_address/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def validIPAddress(self, IP: str) -> str:
+        four = re.compile(r'^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])$')
+        six = re.compile(r'^[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}:[0-9a-fA-F]{1,4}$')
+        if re.search(four, IP):
+            return 'IPv4'
+        elif re.search(six, IP):
+            return 'IPv6'
+        else:
+            return 'Neither'
